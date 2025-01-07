45d8c121e2e15c17aff4413a909e719d8f71ead1
epriou <etienne.priou@systeme-u.fr>
2025-01-07 16:17:37 +0100

    :lipstick: add CallCard

---
+++ app/src/main/java/com/demo/utech/screenshottesting/ui/CallCard.kt
@@ -0,0 +1,45 @@
+package com.demo.utech.screenshottesting.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CallCard(
+    userName: String,
+    modifier: Modifier = Modifier,
+
+    ) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Call,
+                contentDescription = "Call Icon"
+            )
+            Text(userName)
+        }
+    }
+}
+
+@Composable
+@Preview
+fun CallCardPreview() {
+    CallCard("Robin")
+}
---
+++ app/src/screenshotTest/kotlin/com/demo/utech/screenshottesting/CallCardScreenshots.kt
@@ -0,0 +1,14 @@
+package com.demo.utech.screenshottesting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.demo.utech.screenshottesting.ui.CallCardPreview
+
+class CallCardScreenshots {
+    @Preview
+    @Composable
+    fun CallCardPreviewTest() {
+        CallCardPreview()
+    }
+
+}
