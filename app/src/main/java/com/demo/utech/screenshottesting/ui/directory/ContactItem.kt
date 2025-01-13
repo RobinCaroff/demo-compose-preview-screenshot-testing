e332a02f0c17ee5ff0153380e484dd9a0a417765
epriou <etienne.priou@systeme-u.fr>
2025-01-13 17:00:48 +0100

    :art: add complete screen

---
+++ app/src/main/java/com/demo/utech/screenshottesting/ui/directory/ContactItem.kt
@@ -0,0 +1,50 @@
+package com.demo.utech.screenshottesting.ui.directory
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.demo.utech.screenshottesting.ui.Contact
+
+@Composable
+fun ContactItem(
+    contact: Contact,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.padding(4.dp))
+        Icon(Icons.Outlined.AccountBox, contentDescription = "Favoris")
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Text(contact.name)
+            Text(contact.phoneNumber)
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun ContactItemPreview() {
+    ContactItem(
+        Contact("Alice", "1234567890")
+    )
+}
